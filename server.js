a98f61b699b09b6ecc305cd4d711761da94ee226
junryl bejerano <jbejerano@yondu.com>
2023-09-11 20:21:09 +0800

    test

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const hapi = require("@hapi/hapi");
 
 const init = async () => {
     const server = hapi.Server({
-        host: "https://lemon-bass-toga.cyclic.app",
         port: 300
     })
     server.route({
