ad33f4133568a1b5f30864a1153eb515d5dcc451
junryl bejerano <jbejerano@yondu.com>
2023-09-19 13:44:41 +0800

    test

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
-// mongoose.connect(
-//   "mongodb+srv://jsaraba57:D29uk6d2sRjHx89g@cluster0.vxwlj66.mongodb.net/todaDb?retryWrites=true&w=majority"
-// );
+mongoose.connect(
+  "mongodb+srv://jsaraba57:D29uk6d2sRjHx89g@cluster0.vxwlj66.mongodb.net/todaDb?retryWrites=true&w=majority"
+);
 // Start the server on port 3000
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
@@ -16,7 +16,7 @@ app.listen(PORT, () => {
 });
 
 app.get("/", function (req, res) {
-  res.status(200, { msg: "Welcome  new API" });
+  res(200, { msg: "Welcome  new API" });
 });
 
 const UserRoutes = require("./src/features/accounts/routes/accounts");
--- src/features/accounts/handlers/get-accounts.js
+++ src/features/accounts/handlers/get-accounts.js
@@ -1,25 +1,22 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const userSchema = new mongoose.Schema({
-//   name: String,
-// });
+const userSchema = new mongoose.Schema({
+  name: String,
+});
 
 
-// const userModel = mongoose.model("user", userSchema);
+const userModel = mongoose.model("user", userSchema);
 
 module.exports = async function (req, res) {
     try {
-        // testUser = new userModel({
-        //   name:"test2 "
-        // })
-        // testUser.save()
-    // let test = await userModel.find({}).then(function (user) {
-    //     return user
-    // }).catch(function(err){
-    //     console.log(err);
-    // });
-    //     console.log(test);
-    res.send({  });
+
+    let test = await userModel.find({}).then(function (user) {
+        return user
+    }).catch(function(err){
+        console.log(err);
+    });
+        console.log(test);
+    res.send({ test });
   } catch (error) {
     console.log("errr", error);
     // res.send(500, { code: "ResponseStatusFailed", message: error });
